efdb67fa26ef6b77c633a82cf7de9694237116c3
RubalVerma <153887698+RubalVerma@users.noreply.github.com>
2024-06-26 19:52:22 +0530

    Add files via upload

---
+++ SQL/Activities/Activity_7.sql
@@ -0,0 +1,24 @@
+--Activity 7
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+--highest purchase amount ordered by the each customer with their ID and highest purchase amount.
+SELECT customer_id, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id;
+
+--highest purchase amount on '2012-08-17' for each salesman with their ID.
+SELECT salesman_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
+WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesman_id, order_date;
+
+--highest purchase amount with their ID and order date, for only those customers who have a higher purchase amount within the list 2030, 3450, 5760, and 6000.
+SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders
+GROUP BY customer_id, order_date
+HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
